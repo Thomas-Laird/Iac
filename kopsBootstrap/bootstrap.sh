004b3b96dc3e1d9f082b69f8b4faa6fcafb3f4cb
Thomas-Laird <Tomlaird1@gmail.com>
2019-05-08 14:57:30 +0000

    Added Basic Kops setup

---
+++ kopsBootstrap/bootstrap.sh
@@ -0,0 +1,20 @@
+export NAME=test.k8s.local
+export KOPS_STATE_STORE=s3://thomas-laird-kubernetes/kopsState
+
+#kops create cluster --state ${KOPS_STATE_STORE} --name ${NAME} --zones eu-west-1a --target terraform --out .
+
+kops replace -f cluster.yaml --name --state ${KOPS_STATE_STORE} --name ${NAME} --force
+
+kops create secret --name ${NAME} sshpublickey admin -i ~/.ssh/id_rsa.pub
+
+kops update cluster --target terraform ${NAME} --out .
+
+terraform init
+
+terraform apply --auto-approve
+
+kops update cluster --target terraform --name ${NAME} --out .
+
+kops rolling-update cluster --name ${NAME} --cloudonly --force --yes
+
+
