e749e7c8474fd3fbd5f87af4ceeef5b9feb824f6
Thomas-Laird <Tomlaird1@gmail.com>
2019-05-08 15:32:17 +0000

    Broken up cluster config to template and value files. Bootstrap script creates config file from them.

--- kopsBootstrap/bootstrap.sh
+++ kopsBootstrap/bootstrap.sh
@@ -3,6 +3,8 @@ export KOPS_STATE_STORE=s3://thomas-laird-kubernetes/kopsState
 
 #kops create cluster --state ${KOPS_STATE_STORE} --name ${NAME} --zones eu-west-1a --target terraform --out .
 
+kops toolbox template --template template.yaml --values values.yaml > cluster.yaml
+
 kops replace -f cluster.yaml --name --state ${KOPS_STATE_STORE} --name ${NAME} --force
 
 kops create secret --name ${NAME} sshpublickey admin -i ~/.ssh/id_rsa.pub
